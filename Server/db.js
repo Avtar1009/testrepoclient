95f5cfdc719c1b25d1b97e54e298beb2c94f2e9d
Avtar1009 <avtarr0802@gmail.com>
2025-02-08 12:52:50 +0530

    Update server password

--- Server/db.js
+++ Server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //  password is "Ankit@9053", encode the "@" as "%40"
-const connection = mongoose.connect("mongodb+srv://ankit8287670719:Ankit%409053@cluster0.mbtax.mongodb.net/rctserver?retryWrites=true&w=majority");
+const connection = mongoose.connect("mongodb+srv://ankit8287670719:Ankit@cluster0.mbtax.mongodb.net/rctserver?retryWrites=true&w=majority");
 
 module.exports = {
     connection
